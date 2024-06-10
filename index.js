12bdb4e51b4d3ff647933e13c5c811df3ec550ba
salma erramly <101942609+erramlysalma2@users.noreply.github.com>
2024-06-10 19:27:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = '0.0.0.0'; // Modification ici
+const hostname = '0.0.0.0'; // Modifier cette ligne
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -12,4 +12,5 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
 
