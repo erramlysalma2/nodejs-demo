d7e93dbec5f7dada3f88151ed5a007a2750b2da9
SALMA <erramlysalma1@gmail.com>
2024-06-11 13:09:10 +0100

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('This is a nodejs app for demo testtt\n');
+  res.end('This is a nodejs app for demo \n');
 });
 
 server.listen(port, hostname, () => {
